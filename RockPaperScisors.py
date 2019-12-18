e05b10e5fb2d57c2d04b529d60c200f68ecc09dc
Jaquiwa <37348681+Jaquiwa@users.noreply.github.com>
2019-12-18 15:46:07 -0700

    Update RockPaperScisors.py

--- RockPaperScisors.py
+++ RockPaperScisors.py
@@ -89,23 +89,42 @@ def win_conditions(player, computer):
 
 
 def game():
+    # allows replayability without relaunching
     run_till_false = True
+    # runs game until someone reaches the win condition
     winner = False
+    length = 0
     player_score = 0
     comp_score = 0
+    # this array isn't necessary but I wanted to remember how arrays worked hehe
     score = [player_score, comp_score]
     player_inp = 0
     comp_inp = 0
 
     while run_till_false:
-        length = input('What game length would you like... first to: ')
-        while not winner:
+        # to prevent crashing from invalid inputs
+        while True:
+            try:
+                length = int(input('What game length would you like... first to: '))
+                break
+            except ValueError:
+                print('There was an error with your input. ensure you enter in a positive number.')
 
-            player_inp = input('0 - Rock,1 - Paper, or 2 - Scissors?')
-            while int(player_inp) >= 3:
-                player_inp = input('That is not a valid input. 0 - Rock,1 - Paper, or 2 - Scissors?')
+        while not winner:
+            # selecting rock, paper, scissors for player and comp also some measure to prevent Value Errors
+            while True:
+                try:
+                    player_inp = input('0 - Rock,1 - Paper, or 2 - Scissors?')
+                    while int(player_inp) >= 3:
+                        player_inp = input('That is not a valid input. 0 - Rock,1 - Paper, or 2 - Scissors?')
+                    break
+                except ValueError:
+                    print('There was an error with you input, please ensure you enter a number 0-2.')
+                if int(player_inp) >= 3:
+                    player_inp = input('That is not a valid input. 0 - Rock,1 - Paper, or 2 - Scissors?')
             comp_inp = random.randint(0, 2)
             show_moves(int(player_inp), comp_inp)
+            # finding winner and decisions for compensation
             if win_conditions(int(player_inp), comp_inp) == 1:
                 player_score += 1
                 print('you won the round!')
@@ -114,17 +133,31 @@ def game():
                 print('you lost the round!')
             elif win_conditions(int(player_inp), int(comp_inp)) == 3:
                 print('It was a tie! no points awarded.')
+            # will game end? probably not the best way to do this but it works
             score = [player_score, comp_score]
             if score[0] == int(length):
                 winner = True
             elif score[1] == int(length):
                 winner = True
+            # this may be redundent with while loop, but the score was being printed and input required to continue
+            # so I added this to hopefully make winning/losing a little smoother.
+            if winner:
+                break
             print('The current score is', str(score))
             input('press enter to continue...')
         if score[0] == int(length):
             print('You Win! Play again!')
         elif score[1] == int(length):
             print('You Lost, better luck next time.')
+        # was having problems with this part of the game, gonna fix later pretty sure of the problem in my spaghetti.
+        # keep_playing = input('\nWould you like to keep playing? 0 - Yes 1- No')
+        # if int(keep_playing) == 0:
+        #     pass
+        # elif int(keep_playing) == 1:
+        #     run_till_false = False
+        # else:
+        #     print('not a valid input, Exiting Game...')
+        #     run_till_false = False
         run_till_false = False
 
 
